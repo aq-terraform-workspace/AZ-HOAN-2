bfc18814ee7e3f35aa637c6c224fc494f407a000
Anh Quach <anh.quachquituan@halliburton.com>
2021-08-07 01:31:00 +0700

    Add credential module

--- main.tf
+++ main.tf
@@ -1,4 +1,43 @@
-resource "azurerm_resource_group" "example" {
-  name      = "example"
-  location  = var.location
+data "azurerm_key_vault" "myvault" {
+  name                = "${lower(var.name_prefix)}-keyvault"
+  resource_group_name = "${var.name_prefix}-SVRG"
 }
+
+
+# =================================== #
+# PASSWORD AND SSH KEY GENERATION
+# DO NOT DELETE
+# =================================== #
+# Push admin user to keyvault for later use
+resource "azurerm_key_vault_secret" "linux-user" {
+  key_vault_id         = data.azurerm_key_vault.myvault.id
+  name                 = "admin-user"
+  value                = var.admin_username
+
+  tags = {
+    Name        = "admin-user"
+    DeployDate  = replace(timestamp(), "/T.*$/", "")
+  }
+
+  lifecycle {
+    # Value should be ignore changes because our logic is to use the 1st init value only unless this resource is deleted
+    ignore_changes = [tags, value]
+  }
+}
+
+module "windows-password" {
+  source  = "app.terraform.io/aq-tf-cloud/credential/azure"
+  version = "1.0.0"
+  type                = "password"
+  secret_name         = "windows-admin-password"
+  key_vault_id        = data.azurerm_key_vault.myvault.id
+}
+
+module "linux-ssh-key" {
+  source  = "app.terraform.io/aq-tf-cloud/credential/azure"
+  version = "1.0.0"
+  type                = "ssh"
+  secret_name         = "linux-user-private-ssh-key"
+  key_vault_id        = data.azurerm_key_vault.myvault.id
+}
+# =================================== #
--- variables.tf
+++ variables.tf
@@ -2,6 +2,12 @@ variable "location" {
   description = "Azure Location for resources"
 }
 
-variable "subscription_name" {
-  description = "Azure Location for resources"
+variable "name_prefix" {
+  description = "Name prefix for all name"
+  default     = "ANHQUACH"
+}
+
+variable "admin_user" {
+  description = "Admin user for VMs"
+  default     = "remote_admin"
 }
