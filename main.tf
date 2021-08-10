10754cdcddd89f22caa0815c2df8ef05b74682ca
Anh Quach <anh.quachquituan@halliburton.com>
2021-08-10 22:02:20 +0700

    Recreate all VMs

--- main.tf
+++ main.tf
@@ -68,7 +68,7 @@ module "base_network" {
 }
 
 # Create K8S cluster using VMs
-/* module "vm_k8s_cluster" {
+module "vm_k8s_cluster" {
   source  = "git::https://github.com/aq-terraform-modules/terraform-azure-vm-k8s-cluster.git?ref=dev"
 
   resource_group_name = "${local.name_prefix}-k8s"
@@ -93,4 +93,4 @@ module "bastion_vm" {
   os_image_publisher = local.bastion_os_image_info["publisher"]
   os_image_offer = local.bastion_os_image_info["offer"]
   os_image_sku = local.bastion_os_image_info["sku"]
-} */
+}
