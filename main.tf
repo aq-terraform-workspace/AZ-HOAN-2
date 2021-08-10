0a68548011f2733b03374aeaa916226b378f2014
Anh Quach <anh.quachquituan@halliburton.com>
2021-08-11 01:46:04 +0700

    Update using terraform fmt

--- main.tf
+++ main.tf
@@ -74,14 +74,14 @@ module "base_network" {
 
 # Create K8S cluster using VMs
 /* module "vm_k8s_cluster" {
-  source  = "git::https://github.com/aq-terraform-modules/terraform-azure-vm-k8s-cluster.git?ref=dev"
+  source = "git::https://github.com/aq-terraform-modules/terraform-azure-vm-k8s-cluster.git?ref=dev"
 
   resource_group_name = "${local.name_prefix}-k8s"
-  vm_name = "k8s"
-  location = local.location
-  subnet_id = module.base_network.subnet_private_id
-  admin_username = local.admin_username
-  ssh_public_key = module.linux_ssh_key.ssh_public_key
+  vm_name             = "k8s"
+  location            = local.location
+  subnet_id           = module.base_network.subnet_private_id
+  admin_username      = local.admin_username
+  ssh_public_key      = module.linux_ssh_key.ssh_public_key
 
   depends_on = [
     module.base_network
@@ -90,7 +90,7 @@ module "base_network" {
 
 # Create resource group for bastion host
 resource "azurerm_resource_group" "bastion_rg" {
-  name = "${local.name_prefix}-bastion"
+  name     = "${local.name_prefix}-bastion"
   location = local.location
 
   lifecycle {
@@ -100,19 +100,19 @@ resource "azurerm_resource_group" "bastion_rg" {
 
 # Create bastion host using windows VM
 module "bastion_vm" {
-  source  = "git::https://github.com/aq-terraform-modules/terraform-azure-simple-vm.git?ref=dev"
+  source = "git::https://github.com/aq-terraform-modules/terraform-azure-simple-vm.git?ref=dev"
 
   resource_group_name = azurerm_resource_group.bastion_rg.name
-  vm_name = "bastion"
-  location = local.location
-  subnet_id = module.base_network.subnet_public_id
-  os_type = "windows"
-  is_public = true
-  admin_username = local.admin_username
-  admin_password = module.windows_password.value
-  os_image_publisher = local.windows_os_image_info["publisher"]
-  os_image_offer = local.windows_os_image_info["offer"]
-  os_image_sku = local.windows_os_image_info["sku"]
+  vm_name             = "bastion"
+  location            = local.location
+  subnet_id           = module.base_network.subnet_public_id
+  os_type             = "windows"
+  is_public           = true
+  admin_username      = local.admin_username
+  admin_password      = module.windows_password.value
+  os_image_publisher  = local.windows_os_image_info["publisher"]
+  os_image_offer      = local.windows_os_image_info["offer"]
+  os_image_sku        = local.windows_os_image_info["sku"]
 
   depends_on = [
     module.base_network
