30c2a82c5f2dbf9d0b726bf61a5cd78a60abf40c
Anh Quach <anh.quachquituan@halliburton.com>
2021-08-11 01:24:33 +0700

    Add ansible public VM

--- main.tf
+++ main.tf
@@ -200,3 +200,33 @@ module "client_linux" {
     module.base_network
   ]
 }
+
+# Create resource group for ansible awx
+resource "azurerm_resource_group" "ansible" {
+  name = "${local.name_prefix}-ansible"
+  location = local.location
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
+}
+
+module "client_linux" {
+  source  = "git::https://github.com/aq-terraform-modules/terraform-azure-simple-vm.git?ref=dev"
+
+  resource_group_name = azurerm_resource_group.ansible.name
+  vm_name = "ansible-awx"
+  location = local.location
+  subnet_id = module.base_network.subnet_public_id
+  os_type = "linux"
+  is_public = true
+  admin_username = local.admin_username
+  ssh_public_key = module.linux_ssh_key.ssh_public_key
+  os_image_publisher = local.linux_os_image_info["publisher"]
+  os_image_offer = local.linux_os_image_info["offer"]
+  os_image_sku = local.linux_os_image_info["sku"]
+
+  depends_on = [
+    module.base_network
+  ]
+}
