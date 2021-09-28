232344d6ec569831c1f7f5f049af363f45632eb0
Anh Quach (Tony) <anh.quachquituan@halliburton.com>
2021-09-29 01:10:23 +0700

    Add ACI for testing

---
+++ aci.tf
@@ -0,0 +1,39 @@
+module "pierrecardinvn_aci" {
+  source = "git::https://github.com/aq-terraform-modules/terraform-aci.git?ref=dev"
+
+  resource_group_name = "pierrecardinvn.com"
+  location            = local.location
+  aci_name            = "pierrecardinvn.com"
+  dns_name_label      = "pierrecardinvn"
+  container_name      = "aapanel"
+  container_image     = "sheid1309/aapanel:1.2"
+  cpu                 = "2.5"
+  memory              = "3"
+
+  container_ports = [
+    {
+      port     = 80
+      protocol = "TCP"
+    },
+    {
+      port     = 443
+      protocol = "TCP"
+    },
+    {
+      port     = 888
+      protocol = "TCP"
+    },
+    {
+      port     = 8888
+      protocol = "TCP"
+    }
+  ]
+
+  environment_variables = {
+    PORT = 8888
+  }
+
+  secure_environment_variables = {
+    PANEL_PWD = "p@ssw0rd"
+  }
+}
