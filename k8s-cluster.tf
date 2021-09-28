01636ce1307542013fb50d90b2b15efaf49f6524
Anh Quach (Tony) <anh.quachquituan@halliburton.com>
2021-09-29 01:20:28 +0700

    Add var for node http and https port

--- k8s-cluster.tf
+++ k8s-cluster.tf
@@ -7,6 +7,8 @@ module "vm_k8s_cluster" {
   subnet_id           = module.base_network.subnet_private_id
   admin_username      = local.admin_username
   ssh_public_key      = module.linux_ssh_key.ssh_public_key
+  http_node_port      = 30004
+  https_node_port      = 30735
 
   depends_on = [
     module.base_network
