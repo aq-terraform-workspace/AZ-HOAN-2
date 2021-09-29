bfa1e373b2a992312eef76e3dd78e1b38dbd1e79
Anh Quach (Tony) <anh.quachquituan@halliburton.com>
2021-09-29 19:26:53 +0700

    Delete VM

--- aci.tf
+++ aci.tf
@@ -1,4 +1,4 @@
-module "pierrecardinvn_aci" {
+/* module "pierrecardinvn_aci" {
   source = "git::https://github.com/aq-terraform-modules/terraform-aci.git?ref=dev"
 
   resource_group_name  = "pierrecardinvn.com"
@@ -36,4 +36,4 @@ module "pierrecardinvn_aci" {
   secure_environment_variables = {
     PANEL_PWD = "p@ssw0rd"
   }
-}
+} */
