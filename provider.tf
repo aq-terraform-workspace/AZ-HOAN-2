6386a7e01647714c285348bd9b7901a4f46b070d
Anh Quach <anhquach@192.168.2.7>
2023-05-30 14:44:17 +0700

    init

---
+++ provider.tf
@@ -0,0 +1,29 @@
+# Provider configuration
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.58.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  # subscription_id = local.subscription_id
+  # tenant_id       = local.tenant_id
+}
+
+# Backend configuration
+# Replace these values to your own
+terraform {
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "aq-tf-cloud"
+
+    workspaces {
+      name = "AZ-LAB-1"
+    }
+  }
+}
